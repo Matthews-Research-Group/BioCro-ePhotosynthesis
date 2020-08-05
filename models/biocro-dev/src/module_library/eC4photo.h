8852ad53abaa202c647e6aefdc0130f8a072bfd9
Megan L. Matthews <53279181+leighmatth@users.noreply.github.com>
2020-08-05 12:27:10 -0500

    remove biocro pointer

--- models/biocro-dev/src/module_library/eC4photo.h
+++
@@ -1,9 +0,0 @@
-#ifndef EC4PHOTO_H
-#define EC4PHOTO_H
-
-double eC4photoC(double QP, double TEMP, double RH, double CA,
-        double OA, double VCMAX, double VPMAX, double VPR,
-        double JMAX);
-
-#endif
-
--- models/biocro-dev/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(BioCro)
-
-test_check("BioCro")
