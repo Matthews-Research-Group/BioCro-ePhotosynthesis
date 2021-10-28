9ed7e8278cf7583de208ae07e88c39dfd78f1f2e
uphone87 <yufenghe@biologin-0.igb.illinois.edu>
2021-10-28 17:10:47 -0500

    changed run parameters to make inputs easier (&safer)

--- test_ephoto.R
+++ test_ephoto.R
@@ -1,6 +1,11 @@
 library(BioCro)
 #library(reshape2)
 #library(ggplot2)
+restart = TRUE 
+run_days = 30
+run_hours = run_days * 24
+start_day = 242 #starting day of year, minimal from the sowdate! 
+end_day   = start_day+run_days-1
 
 year <- '2002' # other options: '2004', '2005', '2006'
 
@@ -12,11 +17,25 @@ dates <- data.frame("year" = 2001:2006,"sow" = c(143,152,147,149,148,148), "harv
 
 sowdate <- dates$sow[which(dates$year == year)] #143 #148 #149 #152
 harvestdate <- dates$harvest[which(dates$year == year)] #291 #270 #280 #288
-sd.ind <- which(weather$doy == sowdate)[1] + 9
+sd.ind <- which(weather$doy == sowdate)[1]
 hd.ind <- which(weather$doy == harvestdate)[24]
 
+finished_days = start_day - sowdate #count from the sowdate
+if(finished_days<0) stop("incorrect start day!") 
+finished_hours = finished_days *24
+beg_ind = finished_hours+1
+end_ind = finished_hours + run_hours 
+out_filename = paste0("daily_outputs_run2/results_ephoto_",start_day,"_",end_day)
+last_filename = paste0("daily_outputs_run2/results_ephoto_",start_day-30,"_",start_day-1,"_day30.rds")
+
+print(paste("sow,start and end days are",sowdate,start_day,end_day))
+
 weather <- weather[sd.ind:hd.ind,]  #growing season
-weather <- weather[1:24,]  #first day
+if(end_ind > dim(weather)[1]){
+weather <- weather[beg_ind:dim(weather)[1],]  #
+}else{
+weather <- weather[beg_ind:end_ind,]  # subset of the weather for the run period ONLY!
+}
 #weather = read.table("parameter_files/soybean_weather_2002doy202hrs0800_0900.txt",header=TRUE) #two hours
 
 ## Experimentally collected biomassees
@@ -69,9 +88,35 @@ solver_params <- list(
   adaptive_abs_error_tol = 1e-4,
   adaptive_max_steps = 200)
 
-
-result <- Gro_solver(soybean_initial_state, soybean_parameters, weather, steady_state_modules, derivative_modules,solver = solver_params)
-
-saveRDS(result,"results.rds")
+for (i in 1:run_days){
+   if(restart){
+	result_last   = readRDS(last_filename)
+   	init_vars = names(soybean_initial_state)
+   	x_sub = result_last[,init_vars]
+   	last_values = x_sub[dim(x_sub)[1],] 
+   	
+   	soybean_initial_state_last = soybean_initial_state
+   	for (ii in 1:length(soybean_initial_state)){
+   	        soybean_initial_state_last[[ii]] = last_values[ii]
+   	}
+   }else{
+   	soybean_initial_state_last = soybean_initial_state
+   }
+   b0 = (i-1)*24+1
+   b1 = i*24
+   if(b1 > dim(weather)[1]) break  #make sure index not out of range! 
+   weather_i = weather[b0:b1,]
+#for (j in 1:dim(weather_i)[1]){
+#   weather_ij = weather_i[j,]
+   result <- Gro_solver(soybean_initial_state_last, soybean_parameters, weather_i, steady_state_modules, derivative_modules,solver = solver_params)
+#   print(c("finished hour",j))
+#}
+   outfile_i = paste0(out_filename,"_day",i,".rds")
+   saveRDS(result,outfile_i)
+   print(c("finished day",i))
+#read in the results from the last step
+   last_filename = outfile_i 
+   result_last   = readRDS(last_filename)
+}
 
 print("Successfully done!")
