1e9d8d9ad679b56c2b473e950048c986d4494736
Yufeng <yufenghe87@gmail.com>
2023-04-19 23:03:35 -0500

    some clean and comments

--- test_ephoto_v2.R
+++ test_ephoto_v2.R
@@ -16,34 +16,35 @@ weather <- weather[sd.ind:hd.ind,]  #growing season
 #------------------------
 #Currently, NO need to change "restart" since it's automatically addressed below!!
 #
-restart = FALSE #whether the first day of current period of run is RESTART or NOT 
-run_days  = 73#136 
-run_hours = run_days * 24
-start_day = 215#152 #starting day of year, minimal from the sowdate! 
+restart = FALSE # Whether the first day of current period of run is RESTART or NOT 
+run_days  = 136  # Total number of days to run 
+run_hours = run_days * 24 #Total hours
+start_day = 152   #152 #starting day of year, minimal from the sowdate! 
 end_day   = start_day+run_days-1
-output_folder = "results_ephoto_v2"
+output_folder = "results_ephoto_v2_continued_DOY"  #the folder to save daily outputs
 #------------------------
 
 if(start_day>sowdate) restart = TRUE
-
-finished_days = start_day - sowdate #count from the sowdate
+#count from the sowdate; 
+#we need to know how many days finished to index the weather data correctly
+finished_days = start_day - sowdate 
 if(finished_days<0) stop("incorrect start day!") 
 finished_hours = finished_days *24
-beg_ind = finished_hours+1
+beg_ind = finished_hours+1  #begin of index for weather
 #add an extra hour for the next init state 
-#this is because in BioCro, the output of hour x has the label at hour x+1 
-end_ind = min(finished_hours + run_hours +1, dim(weather)[1]) 
-out_filename  = paste0(output_folder,"/results_ephoto_full_",start_day,"_",end_day)
+#this is because in BioCro, the output of hour x has the label at hour x+1! 
+end_ind = min(finished_hours + run_hours +1, dim(weather)[1]) #end of index for weather 
+
+out_filename_prefix  = paste0(output_folder,"/results_ephoto_full_")
 if(restart){
-  last_filename = paste0(output_folder,"/results_ephoto_full_165_264_day50.rds")
+  #if it's restart, then we start with the DOY one day before the start_day
+  last_filename = paste0(output_folder,"/results_ephoto_full_doy",start_day-1,".rds")
 }
 print(paste("sow,start and end days are",sowdate,start_day,end_day))
 
-#if(end_ind > dim(weather)[1]){
-#  weather <- weather[beg_ind:dim(weather)[1],]  #
-#}else{
-  weather <- weather[beg_ind:end_ind,]  # subset of the weather for the run period ONLY!
-#}
+# subset of the weather for the run period ONLY!
+weather_sub <- weather[beg_ind:end_ind,]  
+#
 
 # steady_state_modules
 steady_state_modules <- soybean$direct_modules
@@ -74,6 +75,7 @@ solver_params$type = 'homemade_euler'
 #print(soybean_initial_state_last)
 #stop()
 
+#loop through the number of run days
 for (i in 1:run_days){
    if(restart){
 	result_last   = readRDS(last_filename)
@@ -91,26 +93,21 @@ for (i in 1:run_days){
    }
    b0 = (i-1)*24+1
    b1 = i*24 + 1  #add an extra hour for this day, which will be the init state for the next day
-   b1 = min(b1,dim(weather)[1])  #make sure index not out of range! 
-   weather_dayi = weather[b0:b1,]
-#for (j in 1:dim(weather_i)[1]){
-#   weather_ij = weather_i[j,]
- print(soybean_initial_state_last)
+   b1 = min(b1,dim(weather_sub)[1])  #make sure index not out of range! 
+   weather_dayi = weather_sub[b0:b1,]
+
+   print(soybean_initial_state_last)
    result <- run_biocro(soybean_initial_state_last,
                         soybean_parameters, 
                         weather_dayi, 
                         steady_state_modules,
                         derivative_modules
                        )
-#   print(c("finished hour",j))
-#}
-   outfile_i = paste0(out_filename,"_day",i,".rds")
+   outfile_i = paste0(out_filename_prefix,"doy",weather_dayi$doy[1],".rds")
    saveRDS(result,outfile_i)
    print(c("finished day",i))
-#read in the results from the last step
-   last_filename = outfile_i 
-#   result_last   = readRDS(last_filename)
-   restart = TRUE  #After the first day: sow date, it must be a Restart!!!!!
+   last_filename = outfile_i #change the last_filename to the doy just finsihed 
+   restart = TRUE  #After the first day, it must be a Restart!!!!!
 }
 
 print("Successfully done!")
